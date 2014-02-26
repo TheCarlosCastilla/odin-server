37bdb4d89dfaac2bacd9ac2c5e1e5aa8185d4885
Carlos Castilla <ccastilla89@outlook.com>
2014-02-26 16:34:02 -0500

    Cleaned up some code.

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -44,10 +44,6 @@ class SchedulesController < ApplicationController
 
   def create
     @new_schedule = Schedule.new(schedule_params)
-#    @new_schedule.date = params["@schedule"]["date"]
-#    @new_schedule.time = params["@schedule"]["time"]
-#    @new_schedule.question_id = params["@schedule"]["question_id"]
-#    @new_schedule.user_id = params["@schedule"]["user_id"]
     @new_schedule.sent = false
     @new_schedule.save
 
