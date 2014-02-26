14e8259cf8f5724ed00f6692de0cbafc023f5849
Carlos Castilla <ccastilla89@outlook.com>
2014-02-26 13:22:49 -0500

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Nsfproject::Application.routes.draw do
 
   get '/schedules' => 'schedules#index'
   get '/schedules/new' => 'schedules#new'
+  post '/schedules/new' => 'schedules#create'
   get '/schedules/:user' => 'schedules#get_schedule_by_user'
 
   get '/rules/:user' => 'rules#get_rules_by_user'
