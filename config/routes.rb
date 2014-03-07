242ca1ccbbd712e29753fd5eb32a8f1167d9a833
Carlos Castilla <ccastilla89@outlook.com>
2014-03-07 23:28:08 +0000

    bookkeeping

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Nsfproject::Application.routes.draw do
   get '/users/:uuid' => 'users#request_user_id'
   get '/users' => 'users#index'
 
-
+  get '/index' => 'pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
