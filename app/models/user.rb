eefc2dfcfd6b3c72fa58d843489976ce10d14d53
Carlos Castilla <ccastilla89@outlook.com>
2014-09-04 22:10:26 +0000

    Removed phone number hashing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,27 +3,30 @@ class User < ActiveRecord::Base
 
   belongs_to :valid_user
 
-	def self.next(imei, phone)
-    valid = ValidUser.where(imei: imei)
+	#def self.next(imei, phone)
+    def self.next(imei)
+      valid = ValidUser.where(imei: imei)
 
-		if valid.empty?
-		#	puts "IMEI is not authorized!"
-      puts "Adding IMEI to database!"
-      valid = ValidUser.create(imei: imei, is_registered: false)
-      user = valid.user = self.create(user_id: SecureRandom.uuid, hashed_number: Digest::MD5.hexdigest(phone), is_claimed: true)
-      valid.update(is_registered: true)
-      return user.user_id
-    elsif valid.first.is_registered
-      puts "IMEI is already registered!"
-      return valid.first.user.user_id
-		else
-			if self.where(is_claimed: 'false').empty?
-        user = valid.first.user  = self.create(user_id: SecureRandom.uuid, hashed_number: Digest::MD5.hexdigest(phone), valid_user_id: nil, is_claimed: true)
-			  valid.first.update(is_registered: true)
-        valid.first.save
+      if valid.empty?
+		#puts "IMEI is not authorized!"
+        puts "Adding IMEI to database!"
+        valid = ValidUser.create(imei: imei, is_registered: false)
+        #user = valid.user = self.create(user_id: SecureRandom.uuid, hashed_number: Digest::MD5.hexdigest(phone), is_claimed: true)
+        user = valid.user = self.create(user_id: SecureRandom.uuid, hashed_number: nil, is_claimed: true)
+        valid.update(is_registered: true)
         return user.user_id
-			end
+      elsif valid.first.is_registered
+        puts "IMEI is already registered!"
+        return valid.first.user.user_id
+      else
+ 	    if self.where(is_claimed: 'false').empty?
+          #user = valid.first.user  = self.create(user_id: SecureRandom.uuid, hashed_number: Digest::MD5.hexdigest(phone), valid_user_id: nil, is_claimed: true)
+          user = valid.first.user  = self.create(user_id: SecureRandom.uuid, hashed_number: nil, valid_user_id: nil, is_claimed: true)
+          valid.first.update(is_registered: true)
+          valid.first.save
+          return user.user_id
 		end
+      end
 	end
 
 	def self.to_csv
